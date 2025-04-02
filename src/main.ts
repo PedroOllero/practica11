200c51f1ac7258073c53582967b17087eb99312c
Pedro Ollero <pedroollero1998@gmail.com>
2025-04-02 18:58:36 +0200

    feat: improve IBAN validation feedback and refactor display functions

--- src/main.ts
+++ src/main.ts
@@ -33,38 +33,50 @@ const sacarInfo = (iban: string): boolean => {
         });
       };
       sacarBanco(bancos);
+      
       return true;
     }
+  }else{
+    console.log("Error")
   }
-  
+
   return false;
+
 };
 
-const imprimirParrafo = (encabezado: string, datos: string) => {
+const imprimirDatos = (encabezado: string, datos: string) => {
   let crearTexto = document.createElement("p");
   crearTexto.textContent = `${encabezado}: ${datos}`;
   return crearTexto;
 };
 
+const imprimirParrafo = (texto: string): HTMLParagraphElement => {
+  const crearTexto = document.createElement("p");
+  crearTexto.textContent = `${texto}`;
+  return crearTexto;
+}
+
 const imprimirInfo = () => {
   const contenedor = document.getElementById("resultado");
   if (contenedor && contenedor instanceof HTMLDivElement) {
     if (IBANCorrecto) {
-      let crearTexto = document.createElement("p");
-      crearTexto.textContent = `El formato es correcto`;
-      contenedor.appendChild(crearTexto);
+      const esCorrecto = imprimirParrafo("Este IBAN es correcto")
+      contenedor.appendChild(esCorrecto);
+    }else{
+      const esIncorrecto = imprimirParrafo("Este IBAN es incorrecto")
+      contenedor.appendChild(esIncorrecto);
     }
 
-    const entidadBancaria = imprimirParrafo("Banco", IBANBanco);
+    const entidadBancaria = imprimirDatos("Banco", IBANBanco);
     contenedor.appendChild(entidadBancaria);
 
-    const sucursal = imprimirParrafo("Codigo Sucursal", IBANOficina);
+    const sucursal = imprimirDatos("Codigo Sucursal", IBANOficina);
     contenedor.appendChild(sucursal);
 
-    const control = imprimirParrafo("Dígito Control", IBANdigitoControl);
+    const control = imprimirDatos("Dígito Control", IBANdigitoControl);
     contenedor.appendChild(control);
 
-    const numeroCuenta = imprimirParrafo("Número de cuenta", IBANnumeroCuenta);
+    const numeroCuenta = imprimirDatos("Número de cuenta", IBANnumeroCuenta);
     contenedor.appendChild(numeroCuenta);
   }
 };
@@ -72,17 +84,20 @@ const imprimirInfo = () => {
 const formulario = () => {
   const textInput = document.getElementById("input");
   const boton = document.getElementById("boton");
+  const contenedor = document.getElementById("resultado");
 
   if (
     textInput &&
     textInput instanceof HTMLInputElement &&
     boton &&
-    boton instanceof HTMLButtonElement
+    boton instanceof HTMLButtonElement &&
+    contenedor &&
+    contenedor instanceof HTMLDivElement
   ) {
    boton.addEventListener("click", (event)=> {
+    contenedor.innerHTML = ""
     event.preventDefault()
     const resultado = textInput.value
-    console.log(resultado)
     sacarInfo(resultado)
    })
   }
